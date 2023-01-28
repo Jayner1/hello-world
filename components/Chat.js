7a755f1a5804f6295a5bacce06f7cc76903432ff
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2023-01-27 16:54:55 -0800

    5.3

--- components/Chat.js
+++ components/Chat.js
@@ -2,47 +2,124 @@ import React from 'react';
 import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
- export default class Chat extends React.Component {
-   constructor() {
-     super();
-     this.state = {
-       messages: [],
+const firebase = require('firebase');
+require('firebase/firestore');
+
+export default class Chat extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        messages: [],
+        uid: 0,
+        user: {
+          _id: "",
+          name: "",
+          avatar: "",
+        },
      };
-   }
 
-   componentDidMount() {
+    if (!firebase.apps.length) {
+    firebase.initializeApp({
+        apiKey: "AIzaSyA2pNjh7nP-RNWzL86x38iwpmNFiteJVCU",
+
+        authDomain: "test-3cf65.firebaseapp.com",
+      
+        projectId: "test-3cf65",
+      
+        storageBucket: "test-3cf65.appspot.com",
+      
+        messagingSenderId: "920817637813",
+      
+    });
+  }
+
+  this.referenceChatMessages = firebase.firestore().collection("messages");
+}
+
+componentDidMount() {
      let name = this.props.route.params.name;
-
      this.setState({
        messages: [
-         {
-           _id: 1,
-           text: `Hello developer`,
-           createdAt: new Date(),
-           user: {
-             _id: 2,
-             name: 'React Native',
-             avatar: 'https://placeimg.com/140/140/any',
-           },
-         },
-         {
-           _id: 2,
-           text: `${name} has entered the chat`,
-           createdAt: new Date(),
-           system: true,
-         },
        ],
      });
 
      this.props.navigation.setOptions({ title: name });
+
+     this.referenceChatMessages = firebase.firestore().collection('messages');
+     this.unsubscribe = this.referenceChatMessages.onSnapshot(
+       this.onCollectionUpdate
+     );
+
+     this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+       if (!user) {
+         firebase.auth().signInAnonymously();
+       }
+       this.setState({
+         uid: user.uid,
+         messages: [],
+         user: {
+           _id: user.uid,
+           name: name,
+         },
+       });
+
+       this.unsubscribe = this.referenceChatMessages
+         .orderBy('createdAt', 'desc')
+         .onSnapshot(this.onCollectionUpdate);
+     });
    }
+
+   componentWillUnmount() {
+    this.unsubscribe();
+    this.authUnsubscribe();
+  }
 
    onSend(messages = []) {
-     this.setState((previousState) => ({
-       messages: GiftedChat.append(previousState.messages, messages),
-     }));
-   }
+    this.setState(
+      (previousState) => ({
+        messages: GiftedChat.append(previousState.messages, messages),
+      }),
+      () => {
+        this.addMessage();
+      }
+    );
+  }
 
+  addMessage = () => {
+    const message = this.state.messages[0];
+    this.referenceChatMessages.add({
+      uid: this.state.uid,
+      _id: message._id,
+      text: message.text || '',
+      createdAt: message.createdAt,
+      user: message.user,
+    });
+  };
+
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    // go through each document
+    querySnapshot.forEach((doc) => {
+      // get the QueryDocumentSnapshot's data
+      let data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text,
+        createdAt: data.createdAt.toDate(),
+        user: {
+          _id: data.user._id,
+          name: data.user.name,
+          avatar: data.user.avatar || '',
+        },
+      });
+    });
+    this.setState({
+      messages,
+    });
+  };
+
+
+//Bubble customization
    renderBubble(props) {
      return (
        <Bubble
@@ -58,6 +135,7 @@ import { GiftedChat, Bubble } from 'react-native-gifted-chat';
        />
      );
    }
+
 
    render() {
      let color = this.props.route.params.color;
