19b477bb74b285160504e153e0b560b6429e5a6b
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2023-02-02 14:53:45 -0800

    5.5

--- App.js
+++ App.js
@@ -3,14 +3,12 @@ import React, { Component } from 'react';
 import Start from './components/Start';
 import Chat from './components/Chat';
 
-// import react native gesture handler
 import 'react-native-gesture-handler';
 
-// import react Navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-// Create the navigator
+
 const Stack = createStackNavigator();
 
 export default class App extends Component {
